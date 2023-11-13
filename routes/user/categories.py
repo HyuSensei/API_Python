cdc939a66956c91913611b90b32ad9350d5194c9
PhanTienHuy <huyphan0220@gmail.com>
2023-11-13 08:14:36 +0700

    ud2

--- routes/user/categories.py
+++ routes/user/categories.py
@@ -4,13 +4,13 @@ from controllers.user import CategoryController
 router=APIRouter()
 
 @router.get("/api/v1/categories/{id}",status_code=status.HTTP_200_OK)
-def getCategorySkinCare(
+def getCategory(
     id: int,
     page: int = Query(1, ge=1),
     limit: int = Query(8, ge=1, le=100),
 ):
     try:
-        get_category= CategoryController.handleCategorySkinCare(id,page,limit)
+        get_category= CategoryController.handleCategory(id,page,limit)
         return get_category
     except ValueError as e:
         raise HTTPException(
